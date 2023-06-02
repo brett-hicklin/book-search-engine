99f0d777726f88416c7bf296ff2f808ec2e962d2
Brett Hicklin <hicklin.brett@gmail.com>
2023-06-02 18:57:27 -0400

    redeploying to heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const mongoose = require("mongoose");
 
 const { typeDefs, resolvers } = require("./schemas");
 
